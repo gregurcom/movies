a1a5b2c4e67bdd3efc14f4361f019f597b07b34d
mihai <botanmazuro@gmail.com>
2022-03-14 18:46:55 +0200

    modify movie service

--- src/Service/MovieService.php
+++ src/Service/MovieService.php
@@ -9,22 +9,25 @@ use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class MovieService
 {
-    public function __construct(private Container $container) {}
+    public function __construct(private Container $container, private SluggerInterface $slugger) {}
 
     public function storeImage(Movie $movie, UploadedFile $imagePath)
     {
-        $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+        $originalFilename = pathinfo($imagePath->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $fileName = $safeFilename . '-'. uniqid() . '.' . $imagePath->guessExtension();
 
         try {
             $imagePath
-                ->move($this->container->getParameter('kernel.project_dir') . '/public/uploads', $newFileName);
+                ->move($this->container->getParameter('images_directory'), $fileName);
         } catch (FileException $e) {
             new Response($e->getMessage());
         }
 
-        $movie->setImage('/uploads/' . $newFileName);
+        $movie->setImage($fileName);
     }
 }
