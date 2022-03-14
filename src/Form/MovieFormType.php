1dd78ead4c9d485afadb90d1cc7789da69b614bb
mihai <botanmazuro@gmail.com>
2022-03-14 19:01:59 +0200

    add form constraints

--- src/Form/MovieFormType.php
+++ src/Form/MovieFormType.php
@@ -13,16 +13,43 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Positive;
 
 class MovieFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('rating', IntegerType::class, ['required' => false])
-            ->add('description', TextareaType::class)
-            ->add('image', FileType::class, ['required' => false, 'mapped' => false])
+            ->add('title', TextType::class, [
+                'required' => true,
+                'constraints' => [new Length(['min' => 3, 'max' => 150])]
+            ])
+            ->add('rating', IntegerType::class, [
+                'required' => false,
+                'constraints' => new Positive(),
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => true,
+            ])
+            ->add('image', FileType::class, [
+                'required' => false,
+                'mapped' => false,
+                'constraints' => new Image([
+                    'maxSize' => "10M",
+                    'minWidth' => 200,
+                    'maxWidth' => 5000,
+                    'minHeight' => 200,
+                    'maxHeight' => 5000,
+                    'mimeTypes' => [
+                        "image/jpeg",
+                        "image/jpg",
+                        "image/png",
+                        "image/gif",
+                    ],
+                ]),
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
